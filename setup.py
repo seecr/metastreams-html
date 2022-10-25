e6d58b6bf4db9ae1970a12348308b10473eb6c5e
Johan Jonkers <johan@seecr.nl>
2022-10-25 12:13:46 +0200

    JJ: license

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 ## begin license ##
 #
-# "Meresco Html" is a template engine based on generators, and a sequel to Slowfoot.
+# "Metastreams Html" is a template engine based on generators, and a sequel to Slowfoot.
 # It is also known as "DynamicHtml" or "Seecr Html".
 #
 # Copyright (C) 2008-2009 Seek You Too (CQ2) http://www.cq2.nl
-# Copyright (C) 2011-2013, 2015 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2011-2013, 2015, 2022 Seecr (Seek You Too B.V.) https://seecr.nl
 #
-# This file is part of "Meresco Html"
+# This file is part of "Metastreams Html"
 #
-# "Meresco Html" is free software; you can redistribute it and/or modify
+# "Metastreams Html" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# "Meresco Html" is distributed in the hope that it will be useful,
+# "Metastreams Html" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with "Meresco Html"; if not, write to the Free Software
+# along with "Metastreams Html"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
